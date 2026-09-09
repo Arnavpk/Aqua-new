94806ede7819b67ce9aa79f44afb6cf637ab37f5
Arnavpk <arnavpk@gmail.com>
2026-09-09 15:46:48 +0530

    home page about story

--- app/[location]/page.jsx
+++ app/[location]/page.jsx
@@ -52,6 +52,7 @@ export default async function LocationHome({ params }) {
 
   const strapiLocations = await getAllStrapiLocations();
 
+
   // console.log("STRAPI BADGE:", hotOffers.offers.map((offer) => offer.badge));
 
   return (
